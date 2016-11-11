badd86ddc4940630dc9788b4b16c7d8a05d07eca
Pfifer, Justin <pfifer@amazon.com>
2016-11-11 09:55:02 -0800

    Added Missing License Headers
    Added the missing license headers for all the signal handling/stack
    trace files.

--- aws/utils/signal_handler.h
+++ aws/utils/signal_handler.h
@@ -1,6 +1,15 @@
+// Copyright 2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //
-// Created by Pfifer, Justin on 4/29/16.
+// Licensed under the Amazon Software License (the "License").
+// You may not use this file except in compliance with the License.
+// A copy of the License is located at
 //
+//  http://aws.amazon.com/asl
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
 
 #ifndef AMAZON_KINESIS_PRODUCER_INTERNAL_SIGNAL_HANDLER_H
 #define AMAZON_KINESIS_PRODUCER_INTERNAL_SIGNAL_HANDLER_H
