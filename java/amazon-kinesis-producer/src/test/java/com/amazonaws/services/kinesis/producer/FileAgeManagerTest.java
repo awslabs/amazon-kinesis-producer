55f251f829ff316b76ff9f67103bd3ec15e672f1
Justin Pfifer <pfifer@amazon.com>
2016-11-22 12:22:50 -0800

    Keep File Age Recent to Prevent Cleanup (#82)
    * Keep File Age Recent to Prevent Cleanup
    
    The Kinesis Producer Library extracts its files to a temporary
    directory.  By default that is 'java.io.tmpdir', which maybe subject to
    a cleanup policy.  If certain files are removed the kinesis_producer
    process maybe unable to send data.
    
    This change should keep the age of the files moving forward, and prevent
    temporary cleanup scripts from removing important files.

---
+++ java/amazon-kinesis-producer/src/test/java/com/amazonaws/services/kinesis/producer/FileAgeManagerTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Amazon Software License (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://aws.amazon.com/asl/
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.amazonaws.services.kinesis.producer;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FileAgeManagerTest {
+
+    @Mock
+    private ScheduledExecutorService executorService;
+
+    @Mock
+    private File testFile;
+
+    @Test
+    public void simpleTest() {
+        FileAgeManager manager = new FileAgeManager(executorService);
+        List<File> files = makeFileList();
+        manager.registerFiles(files);
+
+        verify(testFile).getAbsoluteFile();
+        when(testFile.exists()).thenReturn(true);
+        when(testFile.setLastModified(anyLong())).thenReturn(true);
+        manager.run();
+
+        verify(testFile).exists();
+        verify(testFile).setLastModified(anyLong());
+    }
+
+    private List<File> makeFileList() {
+        List<File> files = new ArrayList<>();
+        files.add(testFile);
+        when(testFile.getAbsoluteFile()).thenReturn(testFile);
+        return files;
+    }
+
+    @Test
+    public void missingFileTest() {
+        FileAgeManager manager = new FileAgeManager(executorService);
+        manager.registerFiles(makeFileList());
+
+        when(testFile.exists()).thenReturn(false);
+        manager.run();
+
+        verify(testFile).exists();
+        verify(testFile, never()).setLastModified(anyLong());
+
+    }
+
+}
