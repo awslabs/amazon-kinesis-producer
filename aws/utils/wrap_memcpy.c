48654cb89c71f793fc6d287f580888d8cbae6ea6
Pfifer, Justin <pfifer@amazon.com>
2017-05-22 06:38:00 -0700

    Forgot to change the name of the called wrapper.

--- aws/utils/wrap_memcpy.c
+++ aws/utils/wrap_memcpy.c
@@ -28,5 +28,5 @@ asm(".symver __memcpy_2_2_5, memcpy@GLIBC_2.2.5");
  *
  */
 void *__wrap_memcpy(void* dest, const void* src, size_t n) {
-  return __memcpy_glibc_2_2_5(dest, src, n);
+  return __memcpy_2_2_5(dest, src, n);
 }
