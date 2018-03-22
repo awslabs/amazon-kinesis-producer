2ae3ca9cbafd2aade3eb59d01ec1be51b5eaa092
Pfifer, Justin <pfifer@amazon.com>
2018-03-22 10:14:15 -0700

    Modified spin lock to devolve to standard lock after 1000 spins
    Modified the spin lock to use something like a lock after 1000 spins.
    It uses a condition variable to wake it up when its ticket is called.
    
    Includes some temporary debugging configuration

--- aws/utils/spin_lock.h
+++ aws/utils/spin_lock.h
@@ -15,7 +15,10 @@
 #define AWS_UTILS_SPIN_LOCK_H_
 
 #include <atomic>
-
+#include <cstdint>
+#include <mutex>
+#include <condition_variable>
+#include <array>
 #include <boost/noncopyable.hpp>
 
 namespace aws {
@@ -27,22 +30,81 @@ class TicketSpinLock : boost::noncopyable {
       : now_serving_(0),
         next_ticket_(0) {}
 
+#ifdef DEBUG
+  struct DebugStats {
+    std::uint64_t acquired_count;
+    std::uint64_t acquired_with_lock;
+    std::uint64_t total_spins;
+  };
+
+  void add_acquired();
+  void add_acquired_lock();
+  void add_spin();
+
+  DebugStats get_debug_stats();
+#else
+#define add_acquired();
+#define add_acquired_lock();
+#define add_spin();
+#endif
+
+  
   inline void lock() noexcept {
     size_t my_ticket = next_ticket_.fetch_add(1);
-    while (now_serving_ != my_ticket);
+    std::uint32_t spin_count = 0;
+    do {
+      add_spin();
+      spin_count++;
+      if (spin_count > kMaxSpinCount) {
+        std::unique_lock<std::mutex> lock(lock_for_ticket(my_ticket));
+        cv_for_ticket(my_ticket).wait(lock, [this, &my_ticket] { return now_serving_ == my_ticket; });
+        spin_count = 0;
+        add_acquired_lock();
+      }
+    } while (now_serving_ != my_ticket);
+    add_acquired();
   }
 
   inline void unlock() noexcept {
     now_serving_++;
+    std::unique_lock<std::mutex> lock(lock_for_ticket(now_serving_));
+    cv_for_ticket(now_serving_).notify_all();
   }
 
  private:
+  inline std::size_t lock_shard(const std::size_t& ticket) noexcept {
+    return ticket % kLockShards;
+  }
+  inline std::mutex& lock_for_ticket(const std::size_t& ticket) noexcept {
+    return condition_locks_[lock_shard(ticket)];
+  }
+  inline std::condition_variable& cv_for_ticket(const std::size_t& ticket) noexcept {
+    return unlocked_cv_[lock_shard(ticket)];
+  }
+  static const std::uint32_t kMaxSpinCount = 1000;
+
+  static const std::size_t kLockShards = 13;
+  std::array<std::mutex, kLockShards> condition_locks_;
+  std::array<std::condition_variable, kLockShards> unlocked_cv_;
   std::atomic<size_t> now_serving_;
   std::atomic<size_t> next_ticket_;
+
+
 };
 
 class SpinLock : boost::noncopyable {
  public:
+
+#ifdef DEBUG
+  struct DebugStats {
+    std::uint64_t acquired_count;
+    std::uint64_t acquired_with_lock;
+    std::uint64_t total_spins;
+  };
+  DebugStats get_debug_stats() {
+    return DebugStats();
+  }
+#endif
   inline void lock() noexcept {
     while (!try_lock());
   }
--- aws/utils/test/spin_lock_test.cc
+++ aws/utils/test/spin_lock_test.cc
@@ -33,9 +33,15 @@ void test(const std::string test_name,
   Mutex mutex;
   volatile size_t counter = 0;
   std::vector<aws::thread> threads;
+#ifdef DEBUG
+  std::vector<typename Mutex::DebugStats> debug_stats;
+  debug_stats.resize(num_threads);
+#endif
 
+  std::size_t thread_count = 0;
   while (threads.size() < num_threads) {
-    threads.push_back(aws::thread([&] {
+    std::size_t thread_index = thread_count++;
+    threads.push_back(aws::thread([&, thread_index] {
       aws::utils::sleep_until(start);
       for (size_t i = 0; i < cycles_per_thread; i++) {
         aws::lock_guard<Mutex> lk(mutex);
@@ -43,6 +49,9 @@ void test(const std::string test_name,
           counter++;
         }
       }
+#ifdef DEBUG
+      debug_stats[thread_index] = mutex.get_debug_stats();
+#endif
     }));
   }
 
@@ -58,28 +67,42 @@ void test(const std::string test_name,
   LOG(info) << test_name << ": " << num_threads << " threads: "
             << cycles_per_thread * num_threads / seconds
             << " ops per sec";
+
+#ifdef DEBUG
+  LOG(info) << test_name << ": DebugStats";
+  LOG(info) << "\t"
+            << std::setw(20) << "Acquired Count"
+            << std::setw(20) << "Acquired with Lock"
+            << std::setw(20) << "Total Spins";
+  std::for_each(debug_stats.begin(), debug_stats.end(), [](typename Mutex::DebugStats& d) {
+      LOG(info) << "\t"
+                << std::setw(20) << d.acquired_count
+                << std::setw(20) << d.acquired_with_lock
+                << std::setw(20) << d.total_spins;
+    });
+#endif
 }
 
 } //namespace
 
 BOOST_AUTO_TEST_SUITE(SpinLock)
 
-BOOST_AUTO_TEST_CASE(SpinLock) {
-  for (size_t i : {1, 4, 8}) {
-    test<aws::utils::SpinLock, 100000>("SpinLock", i);
-  }
-}
+// BOOST_AUTO_TEST_CASE(SpinLock) {
+//   for (size_t i : {1, 4, 8}) {
+//     test<aws::utils::SpinLock, 100000>("SpinLock", i);
+//   }
+// }
 
 BOOST_AUTO_TEST_CASE(TicketSpinLock) {
-  for (size_t i : {1, 4, 8}) {
+  for (size_t i : {1, 4, 8, 16, 32}) {
     test<aws::utils::TicketSpinLock, 100000>("TicketSpinLock", i);
   }
 }
 
-BOOST_AUTO_TEST_CASE(StdMutex) {
-  for (size_t i : {1, 4, 8}) {
-    test<aws::mutex, 100000>("std::mutex", i);
-  }
-}
+// BOOST_AUTO_TEST_CASE(StdMutex) {
+//   for (size_t i : {1, 4, 8}) {
+//     test<aws::mutex, 100000>("std::mutex", i);
+//   }
+// }
 
 BOOST_AUTO_TEST_SUITE_END()
