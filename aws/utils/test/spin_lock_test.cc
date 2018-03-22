b7af2c9532ccbb77ef5df8e5f8bda705748dcca8
Pfifer, Justin <pfifer@amazon.com>
2018-03-22 14:00:03 -0700

    Update copyright statements

--- aws/utils/test/spin_lock_test.cc
+++ aws/utils/test/spin_lock_test.cc
@@ -1,4 +1,4 @@
-// Copyright 2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //
 // Licensed under the Amazon Software License (the "License").
 // You may not use this file except in compliance with the License.
