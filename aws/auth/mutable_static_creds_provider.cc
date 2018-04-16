376ed8a19755ea883eb9ad0b8216dbd597d27da3
Pfifer, Justin <pfifer@amazon.com>
2018-04-16 08:37:22 -0700

    Added comments around how versioning is handled.
    Indicate that version is packaged with its credentials, and using
    unsigned integers ensure that this will work if the value wraps around.

--- aws/auth/mutable_static_creds_provider.cc
+++ aws/auth/mutable_static_creds_provider.cc
@@ -39,6 +39,11 @@ void MutableStaticCredentialsProvider::set_credentials(const std::string& akid, 
 
   std::uint64_t next_version = version_ + 1;
 
+  //
+  // Since the credentials are created with the expected next version, and the entire update
+  // is protected by a lock we can't get into a scenario where one of the consumers has
+  // a mismatched version and credentials.  
+  //
   std::shared_ptr<VersionedCredentials> new_credentials = std::make_shared<VersionedCredentials>(next_version, akid, sk, token);
 
   //
@@ -57,6 +62,13 @@ Aws::Auth::AWSCredentials MutableStaticCredentialsProvider::GetAWSCredentials() 
   //
   // Check to see if the credentials have been updated.  If they have load the credentials
   // and update the thread local.
+  //
+  // If the credentials are changing rapidly it's possible that the thread will read an
+  // old version of the credentials.  Should that occur the next read will update to the
+  // most current version.
+  //
+  // This check still works in the very unlikely event that the next_version value
+  // wraps around.  
   //
   if (current_creds.version_ != version_) {
     std::shared_ptr<VersionedCredentials> updated = std::atomic_load(&creds_);
