cad162c8a4725c8be14117c1745b6e4c7324a2fd
Abhishek Yadav Akkem <aakkem@amazon.com>
2024-01-03 11:41:00 -0800

    Fixing bug for addUserRecord

--- java/amazon-kinesis-producer/src/main/java/com/amazonaws/services/kinesis/producer/UserRecord.java
+++ java/amazon-kinesis-producer/src/main/java/com/amazonaws/services/kinesis/producer/UserRecord.java
@@ -76,6 +76,7 @@ public class UserRecord {
         this.data = data;
         this.schema = schema;
     }
+
     public UserRecord(String streamName, String partitionKey, String explicitHashKey, ByteBuffer data, String streamARN, Schema schema) {
         this.streamName = streamName;
         this.partitionKey = partitionKey;
